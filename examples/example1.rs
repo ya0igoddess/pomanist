b90f8ac8e35d07089dfd0a84c2e6582163dda740
ya0igoddess <70133277+ya0igoddess@users.noreply.github.com>
2024-02-14 02:20:42 +0300

    init window in example

--- examples/example1.rs
+++ examples/example1.rs
@@ -1,5 +1,53 @@
 extern crate sdl2;
+extern crate gl;
+extern crate ultraviolet;
 
-fn main() {
+
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::video::GLProfile;
+use pomanist::gl_wrapper::safe::safe_gl as sgl;
+
+
+pub fn main() {
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let gl_attr = video_subsystem.gl_attr();
+    gl_attr.set_context_profile(GLProfile::Core);
+    gl_attr.set_context_version(3, 3);
+
+    let window = video_subsystem.window("rust-sdl2 demo", 800, 600)
+        .position_centered()
+        .opengl()
+        .build()
+        .unwrap();
+
+    let _ctx = window.gl_create_context().unwrap();
+    gl::load_with(|name| video_subsystem.gl_get_proc_address(name) as *const _);
     
+    debug_assert_eq!(gl_attr.context_profile(), GLProfile::Core);
+    debug_assert_eq!(gl_attr.context_version(), (3, 3));
+
+    sgl::ClearColor(0.0, 0.0, 0.6, 1.0);
+    sgl::Clear(gl::COLOR_BUFFER_BIT);
+
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
+    'running: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..} | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                    break 'running
+                },
+                _ => {}
+            }
+        }
+        unsafe {
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
+        
+        window.gl_swap_window();
+        ::std::thread::sleep(::std::time::Duration::new(0, 1_000_000_000u32 / 60));
+    }
 }
--- src/gl_wrapper/texture_object.rs
+++ src/gl_wrapper/texture_object.rs
@@ -1,5 +1,4 @@
-use std::fs::File;
-use std::io::{Error, ErrorKind, Read};
+use std::io::Error;
 
 use gl;
 use imagine::Bitmap;
